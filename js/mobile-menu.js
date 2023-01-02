b9a348f7279796f1a5e5c8a5b0314d80c4541c84
PavloMakarychak <117040148+PavloMakarychak@users.noreply.github.com>
2023-01-02 15:14:38 +0200

    ДЗ 8 підключив скрипт до меню

--- js/mobile-menu.js
+++ js/mobile-menu.js
@@ -1,18 +1,18 @@
-  
+ 
+
+
+
 (() => {
-  const mobileMenuRef = document.querySelector('[data-menu-button]');
-  const openMenuRef = document.querySelector('[data-menu-open]');
-   const closeMenuRef = document.querySelector('[data-menu-close]');
-  // const closeMenuBtn = document.querySelector('.js-close-menu');
-    mobileMenuRef.addEventListener('click', () => {
-const expanded =
-        mobileMenuRef.getAttribute('aria-expanded') === 'true' || false;
-       mobileMenuRef.setAttribute('aria-expanded', !expanded);
-
-      mobileMenuRef.classList.toggle('is-open');
-      openMenuRef.classList.toggle('is-open');
-      closeMenuRef.classList.toggle('is-open');
-  }); 
-})();
-
-
+    const refs = {
+      openMenuRef: document.querySelector('.menu-open'),
+      closeMenuRef: document.querySelector('.mob-menu-close'),
+      menu: document.querySelector('.mob-menu'),
+    };
+  
+    refs.openMenuRef.addEventListener("click", toggleMenu);
+    refs.closeMenuRef.addEventListener("click", toggleMenu);
+  
+    function toggleMenu() {
+      refs.menu.classList.toggle('is-open');
+    }
+  })();
